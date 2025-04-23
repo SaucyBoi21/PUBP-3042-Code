fcca3de1d1e396274978c3dba48bb6d6e8d24b7f
Saahas Swaroop <saahas.swaroop@googlemail.com>
2025-04-22 22:22:36 -0400

    added figures

--- Final/models/utils.py
+++ Final/models/utils.py
@@ -55,27 +55,38 @@ def build_chennai_df(years):
     control_df = control_df.replace(to_replace="Invalid", value=np.nan)
     control_df = control_df.dropna()
     
-    return control_df 
+    return control_df
 
-def build_AQI_time_graph(df, title):
+def build_monthly_df(df):
+    df["timestamp"] = pd.to_datetime(df['Date (LT)'])
+    df.set_index('timestamp', inplace=True)
+    
+    monthly_df = df['AQI'].resample('M').mean()
+    monthly_df = monthly_df.reset_index()
+    
+    return monthly_df
+
+def build_AQI_time_graph(df, x_label, y_label, title, filename):
     key_points = ["2015-01-01 01:00 AM", "2016-01-01 01:00 AM", "2017-01-01 01:00 AM", "2018-01-01 01:00 AM", "2019-01-01 01:00 AM", "2020-01-01 01:00 AM", "2021-01-01 01:00 AM", "2022-01-01 01:00 AM", "2023-01-01 01:00 AM", "2024-01-01 01:00 AM", "2025-01-02 01:00 AM", ]
     labels = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
     df_AQI = df["AQI"].to_numpy()
-    df_datetime = df["Date (LT)"].to_numpy()
+    df_datetime = df[x_label].to_numpy()
     plot = plt.figure(figsize=(22,6))
     plt.plot(df_datetime, df_AQI)
     plt.title(title)
     plt.xlabel('Date')
     plt.ylabel('Value')
-    plt.xticks(key_points, labels)
+    #plt.xticks(key_points, labels)
     for label, (value, color) in aqi_levels.items():
         plt.axhline(y=value, color=color, linestyle='--', linewidth=1.5, alpha=0.8, label=label)
-    plt.legend(title='AQI Levels')
+    plt.legend(title='AQI Levels', loc="best")
+    
+    plt.savefig(filename)
     return plot
 
-def build_DiD_graph(treatment_df, control_df, policy_date='2018-01-01', rolling_window=30):
-    treatment_df['datetime'] = pd.to_datetime(treatment_df['Date (LT)'])
-    control_df['datetime'] = pd.to_datetime(control_df['Date (LT)'])
+def build_DiD_graph(treatment_df, control_df, target_x, filename, policy_date='2018-01-01', rolling_window=30):
+    treatment_df['datetime'] = pd.to_datetime(treatment_df[target_x])
+    control_df['datetime'] = pd.to_datetime(control_df[target_x])
 
     treatment_df = treatment_df.groupby('datetime').mean(numeric_only=True).sort_index()
     control_df = control_df.groupby('datetime').mean(numeric_only=True).sort_index()
@@ -84,8 +95,8 @@ def build_DiD_graph(treatment_df, control_df, policy_date='2018-01-01', rolling_
     control_df['AQI_smooth'] = control_df['AQI'].rolling(window=rolling_window, min_periods=1).mean()
 
     combined = pd.DataFrame({
-        'Delhi': treatment_df['AQI_smooth'],
-        'Chennai': control_df['AQI_smooth']
+        'Delhi': treatment_df['AQI'],
+        'Chennai': control_df['AQI']
     }).dropna()
 
     policy_date = pd.to_datetime(policy_date)
@@ -122,12 +133,13 @@ def build_DiD_graph(treatment_df, control_df, policy_date='2018-01-01', rolling_
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
+    plt.savefig(filename)
     plt.show()
     
-def build_DiD_model(treatment_df, control_df, policy_date = '2018-01-01'):
+def build_DiD_model(treatment_df, control_df, target_x, policy_date = '2018-01-01'):
     # Convert datetime
-    treatment_df['datetime'] = pd.to_datetime(treatment_df['Date (LT)'])
-    treatment_df['datetime'] = pd.to_datetime(treatment_df['Date (LT)'])
+    treatment_df['datetime'] = pd.to_datetime(treatment_df[target_x])
+    treatment_df['datetime'] = pd.to_datetime(treatment_df[target_x])
 
     # Label the datasets
     treatment_df = treatment_df.copy()
